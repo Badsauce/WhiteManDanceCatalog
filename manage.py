acc08e9d7fbca076272f31ef19dd16af9c083210
Joshua Curl <curljosh@msu.edu>
2015-02-07 02:14:16 -0500

    Host 0.0.0.0

--- manage.py
+++ manage.py
@@ -37,7 +37,7 @@ def console():
 
 def run():
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='127.0.0.1', port=port, debug=True)
+  app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
